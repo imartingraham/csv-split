999821fc8befe2167a41f916809173b0e66ff632
Ian Graham <imartingraham@gmail.com>
2014-07-21 09:46:46 -0700

    Added new options to name the split files

--- csv-split.rb
+++ csv-split.rb
@@ -3,6 +3,7 @@ require 'csv'
 
 opts = Trollop::options do
   opt :file_path, "Path to csv file to be split", type: :string
+  opt :new_file_name, "Name of the new files. This will be appended with an incremented number", type: :string, default: 'split_file'
   opt :include_headers, "Include headers in new files", default: true, type: :boolean
   opt :line_count, "Number of lines per file", default: 2500, type: :integer
 end
@@ -12,7 +13,8 @@ file = File.expand_path(opts[:file_path])
 col_data = []
 index = 1
 file_int = 0
-new_file = "converted-files/split_file_#{file_int}.csv"
+new_file_tmp = "converted-files/#{opts[:new_file_name]}_%d.csv"
+new_file = sprintf new_file_tmp, file_int
 headers = [];
 CSV.foreach(file, {headers: true, encoding: "windows-1251:utf-8", quote_char: '"', col_sep: "," }) do |row|
 
@@ -34,7 +36,7 @@ CSV.foreach(file, {headers: true, encoding: "windows-1251:utf-8", quote_char: '"
 
     end
     file_int = file_int + 1
-    new_file = "converted-files/split_file_#{file_int}.csv"
+    new_file = sprintf new_file_tmp, file_int
     col_data = []
 
   end
